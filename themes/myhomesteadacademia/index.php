3d6dd2e3cc05e3ab6d85d13414d463bfa7e97446
treighton <treighton.mauldin@get10up.com>
2020-08-22 18:22:12 -0700

    fixing linting

--- themes/myhomesteadacademia/index.php
+++ themes/myhomesteadacademia/index.php
@@ -5,20 +5,22 @@
  * @package mha
  */
 
-$menu = wp_nav_menu( array(
-	'theme_location' => 'primary',
-	'echo' => false
-) );
+$menu = wp_nav_menu(
+	array(
+		'theme_location' => 'primary',
+		'echo' => false,
+	)
+);
 
 get_header(); ?>
 	<div class="hero">
 		<div class="wrapper">
 			<div class="hero__title">
-				<h1><?php esc_html_e('My Homestead Academia', 'mha') ?></h1>
+				<h1><?php esc_html_e( 'My Homestead Academia', 'mha' ); ?></h1>
 			</div>
 			<div class="hero__nav">
 				<nav>
-					<?php echo wp_kses_post($menu) ?>
+					<?php echo wp_kses_post( $menu ); ?>
 				</nav>
 			</div>
 		</div>
@@ -34,14 +36,13 @@ get_header(); ?>
 							<?php
 								display_schema_image( get_post_thumbnail_id(), 'thumbnail', array( 'class' => 'card__header' ) )
 							?>
-
 						<?php endif; ?>
 
 						<div class="card__meta">
 							<?php display_schema_properties(); ?>
 						</div>
 						<div class="card__content">
-							<h2 itemprop="headline"><a href="<?php echo the_permalink(); ?>"><?php the_title(); ?></a></h2>
+							<h2 itemprop="headline"><a href="<?php echo esc_url( the_permalink() ); ?>"><?php the_title(); ?></a></h2>
 							<div class="excerpt" itemprop="description">
 								<?php the_excerpt(); ?>
 							</div>
